9382a67fdea2d973c73da0d932aa8391c624e88a
Christine Cho <tun66463@temple.edu>
2022-03-26 23:54:29 -0400

    Grab creds and return

--- proofOC/loginWindow.py
+++ proofOC/loginWindow.py
@@ -6,11 +6,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 
-
+import time
 
 def login():
     max = 6
-    attempts = 0
+    attempt = 0
+    userN = ""
+    passW = ""
     chrome_options = Options()
     chrome_options.add_experimental_option("detach", True)
     chrome_options.add_argument('start-maximized')
@@ -20,33 +22,42 @@ def login():
     driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.get("https://tuportal.temple.edu/")
     
+
     while(True):
         
         # Need to get user login info
 
-        try:
-            attempts+=1
-            timeOption = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='logout']")))
-            print("Successful Login")
-            driver.quit()
-            return userN, passW
+        # try:
+        #     attempts+=1
+        #     timeOption = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='logout']")))
+        #     print("Successful Login")
+           
+    
+        #     driver.quit()
+        #     return userN, passW
+        # except:
+        #     tries = max-attempts
+        #     if(tries==0):
+        #         print("Abort.")
+        #         driver.quit()
+        #         return False
+        #     else:
+        #         tryString = "try" if (tries==1) else "tries"
+        #         print("Waiting: %d more %s..." % (tries, tryString))
+
+        try:   
+            timeOption = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[@class='btn btn-default btn-login']")))
+            userN = driver.find_element(By.XPATH, "//input[@id='username']").get_attribute("value")
+            passW = driver.find_element(By.XPATH, "//input[@id='password']").get_attribute("value")
+
         except:
-            tries = max-attempts
-            if(tries==0):
-                print("Abort.")
-                driver.quit()
-                return False
-            else:
-                tryString = "try" if (tries==1) else "tries"
-                print("Waiting: %d more %s..." % (tries, tryString))
+            print("Logged In")
+            
+            return userN, passW
 
 
 
+# userN, passW = login()
 
 
-
-userN, passW = login()
-
-print("Final __ " + userN + " __")
-print("Final __ " + passW + " __")
-
+# print(userN + " " + passW)
