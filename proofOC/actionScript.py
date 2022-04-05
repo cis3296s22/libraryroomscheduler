e5a39efba1411f24666ccc00a016f3f9e8801dad
Christine Cho <tun66463@temple.edu>
2022-04-04 22:53:18 -0400

    look through csv file and schedule rooms

--- proofOC/actionScript.py
+++ proofOC/actionScript.py
@@ -1,25 +1,64 @@
 
 
 
-# from findRoom import Scheduler
-# from datetime import date
-# from datetime import datetime
+
 import datetime as dt
-from threading import currentThread
+import calendar
+from TraverseSite import TraverseSite
 
 
-# As soon as it turns the next day - two days ahead are available for schelding
+def convertDate(date, time, roomSize):
+    select = dt.datetime.strptime(date, "%Y-%m-%d").date()
+    fullDate = select.strftime("%B %-d, %Y")
+    weekday = calendar.day_name[select.weekday()]
+    selectTime = (f"{time} {weekday}, {fullDate}")
+    return selectTime
+
+
+# As soon as it turns the next day - check for new day that opened up
 currentDate = dt.date.today()
+newDate = str(currentDate + dt.timedelta(days=2))
+passW = userN = ""
+booking = []
 
-day1 = currentDate + dt.timedelta(days=1)
-day2 = currentDate + dt.timedelta(days=2)
+# Check if there are any dates in the csv that can be booked
+with open('local_repo/bookings.csv', 'r') as f:
+    cnt = 0
+    results = []
+    for line in f:
+            words = line.strip().split(',')
+            lineObj = words[0]
+            if(cnt==0):
+                userN = lineObj
+                passW = words[1]
+                
+            if(words[0] == newDate):
+                print("Booking for date: " + lineObj)
+                booking = words
+                break
 
-canBook = [day1, day2]
+            else:
+                print("Cant book for date: " + lineObj)
+            cnt = cnt+1
 
-print(canBook[0])
 
-print(canBook[1])
+if booking:
+    link = f"https://charlesstudy.temple.edu/reserve/charles-{booking[2]}"
+    scrapeSite = TraverseSite(link)
 
+    dateTime = convertDate(*booking)
+    bookingString = scrapeSite.findRoom(dateTime)
+
+    if bookingString is not None:
+        print(bookingString)
+        scrapeSite.bookRoom(bookingString, userN, passW)
+    else:
+        print("No rooms available")
+else:
+    print("No bookings to be made")
+    
+
+# "https://charlesstudy.temple.edu/reserve/charles-small"
 # print("Date1 =", d1)
 
 # d2 = dt.datetime.strptime("2022-5-8", "%Y-%m-%d").date()
