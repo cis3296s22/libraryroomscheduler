7680842d783ea744775348125e70342c9f5835f7
Christine Cho <tun66463@temple.edu>
2022-04-12 12:00:36 -0400

    tests

---
+++ tests/loginWindow_test.py
@@ -0,0 +1,10 @@
+from proofOC.loginWindow import login
+
+def test_successLogin(user, passW):
+    userR, passWR = login(user, passW)
+    assert userR == user
+    assert passWR == passW
+
+def test_failedLogin():
+    assert login('tun', 'berberb') == (None, None)
+
