ded792f06a5b6422d98c6771a51c4d8e081b7537
TUbobrien922 <91107633+TUbobrien922@users.noreply.github.com>
2022-04-12 12:37:24 -0400

    Update bookRoom.py

--- proofOC/bookRoom.py
+++ proofOC/bookRoom.py
@@ -1,5 +1,6 @@
 # https://kivy.org/doc/stable/guide/packaging-osx.html
 
+import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
